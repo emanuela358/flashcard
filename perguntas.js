f7a5d240d4af4d42d11994e2bf814a6fff5b0627
emanuela358 <vieira.emanuela@escola.pr.gov.br>
2025-08-15 14:45:39 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,61 +1,61 @@
 criaCartao(
-    'Biologia'
-    'Qual é o maior órgão do corpo humano?'
+    'Biologia',
+    'Qual é o maior órgão do corpo humano?',
     'A pele'
 )
 
 
 criaCartao(
-    'Biologia'
-    'Qual o órgão mais forte do corpo humano?'
+    'Biologia',
+    'Qual o órgão mais forte do corpo humano?',
     'O coração'
 )
 
 
 criaCartao(
-    'BIologia'
-    'Qual o órgão mais frágil do corpo humano?'
+    'BIologia',
+    'Qual o órgão mais frágil do corpo humano?',
     'O cérebro'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Qual é o órgão responsável pela produção de insulina no corpo humano?'
+    'Quimica',
+    'Qual é o órgão responsável pela produção de insulina no corpo humano?',
     'Pâncreas'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Qual é o elemento químico mais abundante na Terra?'
+    'Quimica',
+    'Qual é o elemento químico mais abundante na Terra?',
     'Oxigênio'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Qual é a unidade de medida da quantidade de substância no Sistema Internacional?'
+    'Quimica',
+    'Qual é a unidade de medida da quantidade de substância no Sistema Internacional?',
     'Mol'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Oque é uma reação de oxidação?'
+    'Quimica',
+    'Oque é uma reação de oxidação?',
     'Perda de elétrons por uma substância.'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Qual elemento é essencial para a respiração das plantas?'
+    'Quimica',
+    'Qual elemento é essencial para a respiração das plantas?',
     'Clorofila'
 )
 
 
 criaCartao(
-    'Quimica'
-    'Que tipo de ligação ocorre entre os átomos de hidrogênio e oxigênio na água?'
+    'Quimica',
+    'Que tipo de ligação ocorre entre os átomos de hidrogênio e oxigênio na água?',
     'Covalente'
 )
